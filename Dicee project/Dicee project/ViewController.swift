7a937f7f687f572af95a7b9d5bc32ecefefe5615
EstebanDer <estebanderghazarian@gmail.com>
2025-03-01 19:57:48 -0300

    Dice project done

---
+++ Dicee project/Dicee project/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Dicee project
+//
+//  Created by estu on 01/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+   
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [UIImage(named: "dice1"), UIImage(named: "dice2"), UIImage(named: "dice3"), UIImage(named: "dice4"), UIImage(named: "dice5"), UIImage(named: "dice6")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+    }
+}
+
